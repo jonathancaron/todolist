f5082c95336898330b149a7b45c3f9c9ddb09000
jonathancaron <jonathan.caron04@gmail.com>
2017-11-08 11:58:36 +0100

    reset complet + save nouvelles version

--- js/my.js
+++ js/my.js
@@ -1,41 +1,65 @@
-var getJSON = function(url) {
-return new Promise(function(resolve, reject) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('get', url, true);
-  xhr.responseType = 'json';
-  xhr.onload = function() {
-    var status = xhr.status;
-    if (status == 200) {
-      resolve(xhr.response);
-    } else {
-      reject(status);
+"use strict"
+function getData(){
+let data = {
+  "user" : {
+      "name"      : "john",
+      "email"     : "johnsmith@matrix.com",
+      "password"  : "qsdqsfdssqd",
+      "task"      : [0,1]
+  },
+  "task" : [
+    {
+      "title"         : "Faire à manger",
+      "created_on"    : 1510180290,
+      "deadline"      : 1510180457,
+      "status"        : "todo",
+      "done_on"       : null,
+      "user"          : [0]
+    },
+    {
+      "title"         : "Faire la vaisselle",
+      "created_on"    : 1510180290,
+      "deadline"      : 1510180457,
+      "status"        : "done",
+      "done_on"       : null,
+      "user"          : [0]
+    },
+    {
+      "title"         : "Sortir les poubelles",
+      "created_on"    : 1510180290,
+      "deadline"      : 1510180457,
+      "status"        : "late",
+      "done_on"       : null,
+      "user"          : [0]
     }
-  };
-  xhr.send();
-});
-};
+  ]
+}
+return data;
+}
+function updateData(){
 
+}
+function setUser(nickname){
+  document.getElementById('prenom').innerHTML = nickname;
+}
+function setTask(task){
+  console.log(task);
+  if(task.done == false){
 
+  }
+  else{
 
-getJSON('./json/fr.json').then(function(data) {
-  for (var i = 0; i < data.length; i++) {
-    document.getElementById('result').innerHTML += "<div style='bloc'";
-    result.innerText += ' ' + data[i].prenom;
-    document.getElementById('result').innerHTML += '<br>';
-    for (var i2 = 0; i2 < data[i].taches.length; i2++) {
-      result.innerText += ' ' + data[i].taches[i2].tache;
-      result.innerText += ' ' + data[i].taches[i2].date;
-      result.innerText += ' ' + data[i].taches[i2].datefin;
-      var stat = "Non fait";
-      if(data[i].taches[i2].status!=0)
-      { stat = "Fait"; }
-      result.innerText += ' ' + stat;
-      document.getElementById('result').innerHTML += '<br>';
-    }
-    document.getElementById('result').innerHTML += "</div>";
-    document.getElementById('result').innerHTML += '<br>';
+  }
+}
+const app = function(data){
+  let nickname = data.user.name;
+  setUser(nickname);
+
+  let tasks = data.task;
+  for(let i in tasks){
+    setTask(tasks[i]);
   }
 
-}, function(status) {
-alert('Le fichier JSON est introuvable.');
-});
+}
+
+app(getData());
