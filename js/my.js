a2b85a0fb7e5fa115aee065587311b68ef531502
jonathancaron <jonathan.caron04@gmail.com>
2017-11-09 09:14:03 +0100

    update todolist

--- js/my.js
+++ js/my.js
@@ -1,84 +1,103 @@
-"use strict"
+"use strict";
 
-
+// DATA MANAGEMENT //
 function getData(){
-let data = {
-  "user" : {
-      "name"      : "john",
-      "email"     : "johnsmith@matrix.com",
-      "password"  : "qsdqsfdssqd",
-      "task"      : [0,1]
-  },
-  "task" : [
-    {
-      "title"         : "Faire à manger",
-      "created_on"    : 1510180290,
-      "deadline"      : 1510180457,
-      "done_on"       : null,
-      "user"          : [0]
-    },
-    {
-      "title"         : "Faire la vaisselle",
-      "created_on"    : 1510180290,
-      "deadline"      : 1510180457,
-      "done_on"       : 1510180457,
-      "user"          : [0,1]
-    },
-    {
-      "title"         : "Sortir les poubelles",
-      "created_on"    : 1510180290,
-      "deadline"      : 1510180457,
-      "done_on"       : 1510180567,
-      "user"          : [0]
-    }
-  ]
+	// Load data from database sql / nosql / json
+	let data = {
+		"user" : {
+			"name" 		: "john",
+			"email" 	: "johnsmith@matrix.com",
+			// "password" 	: "sjdsjdhu2h2njjsd",
+			// "task" 		: [0,1]
+		},
+		"task" : [
+			{
+				"title" 		: "Faire Ã  manger",
+				"created_on" 	: 1510180290,
+				"deadline"		: 1510180457,
+				"done_on"		: null,
+				"user" 			: [0]
+			},
+			{
+				"title" 		: "Faire la vaisselle",
+				"created_on" 	: 1510180290,
+				"deadline"		: 1510180457,
+				"done_on"		: 1510180457,
+				"user" 			: [0,1]
+			},
+			{
+				"title" 		: "Sortir les poubelles",
+				"created_on" 	: 1510180290,
+				"deadline"		: 1510180457,
+				"done_on"		: 1510180567,
+				"user" 			: [0]
+			}
+		]
+	}
+
+	return data;
 }
-return data;
-}
+
 function updateData(){
+	// update data to database sql / nosql / json
+
 }
 
 function setUser(nickname){
-  document.getElementById('prenom').innerHTML = nickname;
+	document.getElementById("prenom").innerHTML = nickname;
 }
 
 function setTask(task){
-  function display(status){
-    let node = document.createElement('li');
-    node.className = "list-item";
-    let textnode = document.createTextNode(task.title);
-    node.appendChild(textnode);
-    document.getElementById(status).appendChild(node);
+	// console.log(task);
+	function display(status){
+		let node = document.createElement('li'); // Create a <li>
+		node.className = "list-item";
+		let textnode = document.createTextNode(task.title); // Create a text
+		node.appendChild(textnode); // Append the text to the node <li>
+		document.getElementById(status).appendChild(node); // Append the node <li> to element #todo
+	}
+
+	let status = task.done_on != null ? ( task.done_on <= task.deadline ? "done" : "late" ) : "todo";
+	display(status);
+}
+
+function endTask(todoItem){
+	console.log(todoItem)
+  let status = null;
+  if(true){
+    status = document.getElementById("done");
   }
-  console.log(task);
-  if(task.done_on === null)
-    {display("todo");}
   else {
-    if(task.done_on <= task.deadline)
-      {display("done");}
-    else
-      {display("late");}
+    status = document.getElementById("late");
   }
+  status.appendChild(this);
 }
-function endTask(i){
-  console.log(i);
-}
+
+// APP
 const app = function(data){
-  let nickname = data.user.name;
-  setUser(nickname);
+	// User
+	let nickname = data.user.name;
+	setUser(nickname);
 
-  let tasks = data.task;
-  for(let i in tasks){
-    setTask(tasks[i]);
-    alert(i);
-  }
+	// Task
+	let tasks = data.task;
+	for(let i in tasks){
+		setTask(tasks[i]);
+	}
 
-// events
-  let todoItems = document.getElementById("todo").getElementsByClassName("list-item");
-  let i = 0;
-  while (i < todoItems.length) {
-    i++;
-  }
+	// Events listener
+	let todo = document.getElementById("todo");
+	// console.log(todo);
+	let todoItems = todo.getElementsByClassName("list-item");
+	// console.log(todoItems);
+	return false;
+	let i = 0;
+	while( todoItem[i] ){
+		// console.log(todoItem[i])
+		todoItem[i].addEventListener( "click",endTask( todoItem[i]));
+		i++;
+	}
 }
 
+// Run App
 app(getData());
