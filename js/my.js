e234fd291bcac32a57fffd27a97e20fba280710d
jonathancaron <jonathan.caron04@gmail.com>
2017-11-09 15:23:01 +0100

    avancement checkstatus todoclist click

--- js/my.js
+++ js/my.js
@@ -1,103 +1,59 @@
+/*methode endtask, dire que la tache est terminée (click*/
 "use strict";
 
-// DATA MANAGEMENT //
-function getData(){
-	// Load data from database sql / nosql / json
-	let data = {
-		"user" : {
-			"name" 		: "john",
-			"email" 	: "johnsmith@matrix.com",
-			// "password" 	: "sjdsjdhu2h2njjsd",
-			// "task" 		: [0,1]
-		},
-		"task" : [
-			{
-				"title" 		: "Faire Ã  manger",
-				"created_on" 	: 1510180290,
-				"deadline"		: 1510180457,
-				"done_on"		: null,
-				"user" 			: [0]
-			},
-			{
-				"title" 		: "Faire la vaisselle",
-				"created_on" 	: 1510180290,
-				"deadline"		: 1510180457,
-				"done_on"		: 1510180457,
-				"user" 			: [0,1]
-			},
-			{
-				"title" 		: "Sortir les poubelles",
-				"created_on" 	: 1510180290,
-				"deadline"		: 1510180457,
-				"done_on"		: 1510180567,
-				"user" 			: [0]
-			}
-		]
-	}
+let App = {
+	config : {
+		"nickname" : null,
+		"task" : []
+	},
+	setTask : function(taskID){
+		let now  = Math.floor(Date.now() / 1000);
+		this.task[taskID].done_on = now;
+	},
+	start : function(){
+		console.log('Starting Application');
 
-	return data;
-}
+		// Adding nickname to html
+		let n = document.getElementById("prenom");
+			n.innerHTML = this.config.nickname;
 
-function updateData(){
-	// update data to database sql / nosql / json
+		let i = 0;
+		while (this.config.task[i]){
 
-}
+			let t = this.config.task[i];
+			let status = t.done_on != null ? ( t.done_on <= t.deadline ? "done" : "late" ) : "todo";
 
-function setUser(nickname){
-	document.getElementById("prenom").innerHTML = nickname;
-}
+			let htmlElem = document.createElement('li'); // Create a <li>
 
-function setTask(task){
-	// console.log(task);
-	function display(status){
-		let node = document.createElement('li'); // Create a <li>
-		node.className = "list-item";
-		let textnode = document.createTextNode(task.title); // Create a text
-		node.appendChild(textnode); // Append the text to the node <li>
-		document.getElementById(status).appendChild(node); // Append the node <li> to element #todo
-	}
-
-	let status = task.done_on != null ? ( task.done_on <= task.deadline ? "done" : "late" ) : "todo";
-	display(status);
-}
-
-function endTask(todoItem){
-	console.log(todoItem)
-  let status = null;
-  if(true){
-    status = document.getElementById("done");
-  }
-  else {
-    status = document.getElementById("late");
-  }
-  status.appendChild(this);
-}
-
-// APP
-const app = function(data){
-	// User
-	let nickname = data.user.name;
-	setUser(nickname);
-
-	// Task
-	let tasks = data.task;
-	for(let i in tasks){
-		setTask(tasks[i]);
-	}
-
-	// Events listener
-	let todo = document.getElementById("todo");
-	// console.log(todo);
-	let todoItems = todo.getElementsByClassName("list-item");
-	// console.log(todoItems);
-	return false;
-	let i = 0;
-	while( todoItem[i] ){
-		// console.log(todoItem[i])
-		todoItem[i].addEventListener( "click",endTask( todoItem[i]));
-		i++;
+			htmlElem.className = "list-item";
+			htmlElem.ID = t.ID;
+			htmlElem.deadline = t.deadline;
+			let textHtmlElem = document.createTextNode(t.title); // Create a text
+			htmlElem.appendChild(textHtmlElem); // Append the text to the node <li>
+			document.getElementById(status).appendChild(htmlElem); // Append the node <li> to element #todo
+			htmlElem.addEventListener('click', function(event) {
+					alert(t.title);
+			});
+			console.log('Add Task Object: ID - ' + i + ', Object = ' + t.title);
+			i++;
+		}
 	}
 }
 
-// Run App
-app(getData());
+
+App.config.nickname = 'John';
+App.config.task = [
+	{
+		"title" 		: "Faire Ã  manger",
+		"created_on" 	: 1510180290,
+		"deadline"		: 1510180457,
+		"done_on"		: null
+	},
+	{
+		"title" 		: "Regarder la tv",
+		"created_on" 	: 1510156092,
+		"deadline"		: 1510156192,
+		"done_on"		: null
+	}
+]
+App.start();
